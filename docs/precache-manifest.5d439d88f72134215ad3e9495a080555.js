4a672f2565d71f87f7b3d9f010751f2340969710
Benjamin Forster <benjamin.forster@reaktor.com>
2020-09-29 23:01:42 +0300

    add docs directory for github pages

---
+++ docs/precache-manifest.5d439d88f72134215ad3e9495a080555.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4ba3187e67a61d3a65b1ea1240c06ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e3019053c68ba22c283",
+    "url": "/static/css/main.3d72674c.chunk.css"
+  },
+  {
+    "revision": "e3058f4dab52db87220d",
+    "url": "/static/js/2.aca04f5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aca04f5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e3019053c68ba22c283",
+    "url": "/static/js/main.4d8e2cb0.chunk.js"
+  },
+  {
+    "revision": "3002cd1b12d139e688a9",
+    "url": "/static/js/runtime-main.d7f72a4d.js"
+  }
+]);
